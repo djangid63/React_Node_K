14c86775a85071a76fe540aa00b96d97fc7043a5
Devesh Jangid <129510566+djangid63@users.noreply.github.com>
2024-12-20 10:56:25 +0530

    refactor: update image rendering in CardComponents for improved layout and consistency

--- Day04/src/components/CardComponents.jsx
+++ Day04/src/components/CardComponents.jsx
@@ -5,14 +5,14 @@ const CardComponents = ({ product }) => {
     <div className='w-[50%] h-[70%] bg-slate-200 gap-20  p-10 rounded-b-3xl grid grid-cols-2'>
       <div className='flex gap-5 justify-start'>
         <div className=''>
-          <img className='w-32 h-32 mx-4 rounded-full object-scale-down' src='https://media.istockphoto.com/id/964111894/vector/poster-of-the-vinyl-record-vector-illustration-on-dark-background.jpg?s=2048x2048&w=is&k=20&c=D8Oi3fRPP0ieUqZgxrUgq2YMT38yt8nqohE2YzTjTzY=' ></img>
+          <img className='w-32 h-32 mx-4 rounded-full object-cover' src='https://media.istockphoto.com/id/964111894/vector/poster-of-the-vinyl-record-vector-illustration-on-dark-background.jpg?s=2048x2048&w=is&k=20&c=D8Oi3fRPP0ieUqZgxrUgq2YMT38yt8nqohE2YzTjTzY=' ></img>
           <img className='w-32 h-32 mx-4  my-8 rounded-full object-scale-down' src='https://media.istockphoto.com/id/845844762/vector/poster-of-the-vinyl-record-vector-illustration-on-dark-background.jpg?s=1024x1024&w=is&k=20&c=AOe_QTKGLSEytxl2oQA9qjSe1mvt9GvxHCZcgtTaQZ4=' ></img>
-          <img className='w-32 h-32 mx-4 rounded-full object-scale-down' src='https://media.istockphoto.com/id/845844762/vector/poster-of-the-vinyl-record-vector-illustration-on-dark-background.jpg?s=1024x1024&w=is&k=20&c=AOe_QTKGLSEytxl2oQA9qjSe1mvt9GvxHCZcgtTaQZ4=' ></img>
+          <img className='w-32 h-32 mx-4 rounded-full object-cover' src='https://media.istockphoto.com/id/1975058884/vector/modern-abstract-blue-pink-and-purple-gradient-circle-line-on-dark-black-background-design.jpg?s=2048x2048&w=is&k=20&c=aL4cf6InXpsZwm3-CNnS5JJE-QoIbXQvOyNTl-DMMT0=' ></img>
         </div>
         <div className=''>
-          <img className='w-32 h-32 rounded-full object-scale-down' src='https://media.istockphoto.com/id/845844160/vector/poster-of-the-vinyl-record-vector-illustration-on-dark-background.jpg?s=1024x1024&w=is&k=20&c=Y11nqKc1moU_w7J_IvPoe56MDlhJ4MPdn7DHT2FiM9A=' ></img>
+          <img className='w-32 h-32 rounded-full object-cover' src='https://media.istockphoto.com/id/845844160/vector/poster-of-the-vinyl-record-vector-illustration-on-dark-background.jpg?s=1024x1024&w=is&k=20&c=Y11nqKc1moU_w7J_IvPoe56MDlhJ4MPdn7DHT2FiM9A=' ></img>
           <img className='w-32 h-32 my-8  rounded-full object-cover' src='https://media.istockphoto.com/id/1206894561/photo/headphones-with-vinyl-record-80s-synth-wave-and-retrowave-glowing-circle-futuristic.jpg?s=2048x2048&w=is&k=20&c=tPpBLC2fUFamF-Ubmr3xbJbFTUVAmiBxGYCxhqYQu4A=' ></img>
-          <img className='w-32 h-32 rounded-full object-cover' src='https://media.istockphoto.com/id/1206894561/photo/headphones-with-vinyl-record-80s-synth-wave-and-retrowave-glowing-circle-futuristic.jpg?s=2048x2048&w=is&k=20&c=tPpBLC2fUFamF-Ubmr3xbJbFTUVAmiBxGYCxhqYQu4A=' ></img>
+          <img className='w-32 h-32 rounded-full object-cover' src='https://media.istockphoto.com/id/183680582/vector/vinyl-record-vector.jpg?s=2048x2048&w=is&k=20&c=Eh39Nt2l-5ix1kAEGLoL0-qe6BQAUe-WuXqoooBRLQo=' ></img>
         </div>
       </div>
       <div className='flex flex-col gap-12 justify-center items-start w-56 '>
