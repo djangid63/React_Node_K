dcb5c2b14868efb860dbe3fc267ff6f5ec646f3f
Devesh Jangid <129510566+djangid63@users.noreply.github.com>
2025-01-06 11:10:26 +0530

    refactor: improve layout and styling of CardComponents and Navbar for better responsiveness

--- Day04/src/App.jsx
+++ Day04/src/App.jsx
@@ -6,7 +6,7 @@ import products from './utility/data'
 
 function App() {
   return (
-    <div className=' flex flex-col w-screen h-screen  justify-center items-center bg-gray-500 '>
+    <div className=' flex flex-col w-screen h-screen justify-center items-center bg-gray-500 '>
       <Navbar></Navbar>
       <CardComponents product={products} ></CardComponents>
     </div>
--- Day04/src/components/Navbar.jsx
+++ Day04/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='flex flex-col w-[50%] bg-gray-100 p-8 rounded-t-2xl items-start font-extrabold  '>
+    <div className='flex flex-col w-[70%] bg-gray-100 p-8 rounded-t-2xl items-start font-extrabold  '>
       <p className='mr-[21rem] text-2xl'>マルチメーター</p>
       <h1 className='text-8xl'>Electronics</h1>
     </div>
