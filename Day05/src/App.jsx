c3f95f700cdebedeb2dcc7dddc4411df0f79e9ab
Devesh Jangid <129510566+djangid63@users.noreply.github.com>
2025-01-12 23:38:54 +0530

    feat: add ProductCard1 component and product data for displaying popular items

--- Day05/src/App.jsx
+++ Day05/src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import HeroSection from "./components/HeroSection.jsx";
+import ProductCard1 from "./components/ProductCards/ProductCard1.jsx";
+import productData from "./Data/Data";
 
 function App() {
+  // console.log(productData);
   return (
-    <div>
+    <div className="overflow-x-hidden">
       <HeroSection />
+      <ProductCard1 productData={productData} />
     </div>
   );
 }
---
+++ Day05/src/Data/Data.js
@@ -0,0 +1,41 @@
+import ProductImg1 from "../images/PopProductImgs/Product1.jpeg";
+import ProductImg2 from "../images/PopProductImgs/Product2.jpeg";
+import ProductImg3 from "../images/PopProductImgs/Product3.jpeg";
+import ProductImg4 from "../images/PopProductImgs/Product4.jpeg";
+
+const productData = [
+  {
+    title: "Work &amp",
+    subtitle: "Timeless Classic Collection",
+    price: 125,
+    img: ProductImg1,
+    sizes: ["XL", "L", "M", "S", "XS"],
+    colors: ["black", "red", "green"]
+  },
+  {
+    title: "Casual",
+    subtitle: "Bohemian Rhapsody Attire",
+    price: 155,
+    img: ProductImg2,
+    sizes: ["L", "M", "S", "XS"],
+    colors: ["black", "red", "green", "white"]
+  },
+  {
+    title: "Evening Dresses",
+    subtitle: "Midnight Gala Maxi Dress",
+    price: 175,
+    img: ProductImg3,
+    sizes: ["XL", "L", "M", "S", "XS"],
+    colors: ["black", "red", "green", "white"]
+  },
+  {
+    title: "Casual",
+    subtitle: "Power Suit Ensemble",
+    price: 120,
+    img: ProductImg4,
+    sizes: ["XL", "L", "M", "S", "XS"],
+    colors: ["black", "red", "green", "white"]
+  }
+]
+
+export default productData;
