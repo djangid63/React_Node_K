04221cf62fb5f929874b6a5179d4947ebd0a7f5e
Devesh Jangid <129510566+djangid63@users.noreply.github.com>
2025-01-14 18:19:19 +0530

    feat: add new SVG icons and implement ReinventedSection and NewProductCard components for enhanced product display

--- Day05/src/App.jsx
+++ Day05/src/App.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import HeroSection from "./components/HeroSection/HeroSection.jsx";
 import ProductCard1 from "./components/ProductSection/ProductCard1.jsx";
 import productData from "./Data/Data";
+import newProductData from "./Data/NewProduct.jsx";
 import BagSection from "./components/ProductSection/BagSection.jsx";
 import BlazerSection from './components/ProductSection/BlazerSection';
+import ReinventedSection from "./components/ProductSection/ReinventedSection.jsx";
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <ProductCard1 productData={productData} />
       <BagSection />
       <BlazerSection />
+      <ReinventedSection />
     </div>
   );
 }
